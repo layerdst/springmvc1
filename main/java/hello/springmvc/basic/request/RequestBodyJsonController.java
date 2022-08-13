c9434d1805bcd308018ed29f8da129fd98f26806
layerdst <bccfcc@hanmail.net>
2022-08-13 21:43:59 +0900

    Example Project

--- main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -78,5 +79,8 @@ public class RequestBodyJsonController {
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return helloData;
     }
+
+
+
 
 }
--- main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
+++ main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -32,9 +31,61 @@ public class BasicItemController {
         return "/basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 21200, 20));
     }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                        @RequestParam int price,
+                        @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동추가가 되기 때문에 생략이 가능하다.
+
+        return "basic/item";
+    }
+
+    public String addItemV3(@ModelAttribute Item item) { // 클래스명의 첫글자를 소문자로 바꾼  Item -> item 이 model.addAttribute("item" , item) 으로 설정된다.
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동추가가 되기 때문에 생략이 가능하다.
+
+        return "basic/item";
+    }
+
+    public String addItemV4(Item item) { //modelAttribute 자동적용됨됨        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동추가가 되기 때문에 생략이 가능하다.
+
+        return "basic/item";
+    }
+
+
 }
